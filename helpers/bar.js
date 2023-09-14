00185a692542519da6ac5d4eb8dea84df1dbf25f
NeoXavier <xavierneo88@gmail.com>
2023-09-14 13:28:07 +0800

    Comments and documentation

--- helpers/bar.js
+++ helpers/bar.js
@@ -1,16 +1,28 @@
-// min and max values of the overall dataset
+/*
+Description: Bar class for each individual bar in the bar chart
+Inputs: 
+    - Name of the bars
+    - Value of the bars
+    - Position of the bar respective to other bars(eg. 1st, 2nd, 3rd, etc.)
+    - Minimum value of the bars (i.e. lowest value out of all the bars)
+    - Maximum value of the bars (i.e. highest value out of all the bars)
+    - Layout of the chart
+    - Y positions of all the bars
+*/
 function Bar(_name, _value, _position, min, max, layout, y_positions) {
+    // Public variables
     this.name = _name;
     this.value = _value;
     this.position = _position;
-    //this.color = get_color();
     this.color = color(random(0, 200), random(155, 255), random(155, 255));
-    //this.color = color(random(0, 255), random(0, 255), random(0, 255));
+    this.layout = layout;
 
-    var layout = layout;
+    // Private variables
+    var min = min;
+    var max = max;
     var y_positions = y_positions;
-    var plotWidth = layout.plotWidth();
 
+    var plotWidth = this.layout.plotWidth();
     var barHeight = get_bar_height(y_positions);
     var targetBarWidth;
     var currentBarWidth = 0;
@@ -19,8 +31,9 @@ function Bar(_name, _value, _position, min, max, layout, y_positions) {
     var currentY = 0;
     var yChangeDirection = 1;
 
+    // Draw the bar
     this.draw = function() {
-        let x = layout.leftMargin + layout.pad;
+        let x = this.layout.leftMargin + this.layout.pad;
         fill(this.color);
         rect(x, currentY, currentBarWidth, barHeight);
         fill(50);
@@ -30,9 +43,11 @@ function Bar(_name, _value, _position, min, max, layout, y_positions) {
         text(round(this.value), x + currentBarWidth + 10, currentY + barHeight / 2);
     }
 
+    // Update Y position and width of the bar incrementally to create a smooth transition
     this.update = function() {
+        // If the bar is not yet drawn, set the current width and y position to the target width and y position
         if (currentBarWidth == 0) {
-            currentBarWidth = round(map(this.value, min * 0.9, max * 1.1, 100, plotWidth - layout.pad));
+            currentBarWidth = round(map(this.value, min * 0.9, max * 1.1, 100, plotWidth - this.layout.pad));
             targetBarWidth = currentBarWidth;
         }
         if (currentY == 0) {
@@ -40,40 +55,55 @@ function Bar(_name, _value, _position, min, max, layout, y_positions) {
             targetY = currentY;
         }
 
+        // If the bar width or y position is not yet at the target, incrementally change the width and y position
         if (currentBarWidth != targetBarWidth) {
             currentBarWidth += 5 * widthChangeDirection;
+            // "Snap" the bar width to the target width if the difference between the current width and target width is less than 5
             if (abs(currentBarWidth - targetBarWidth) < 5) {
                 currentBarWidth = targetBarWidth;
             }
         }
         if (currentY != targetY) {
             currentY += 2 * yChangeDirection;
+            // "Snap" the y position to the target y position if the difference between the current y position and target y position is less than 5
             if (abs(currentY - targetY) < 5) {
                 currentY = targetY;
             }
         }
     }
 
-    this.changeBarParams = function(ranking, value, new_min, new_max, y_positions, layout) {
+    // Function to change parameters of the bar
+    // Inputs: New ranking of bar, new value, new min and max values of the overall data, new y positions of the bars
+    this.changeBarParams = function(ranking, value, new_min, new_max, new_y_positions) {
         this.position = ranking;
         this.value = value;
-        targetBarWidth = round(map(this.value, new_min * 0.9, new_max * 1.1, 100, plotWidth - layout.pad));
+        y_positions = new_y_positions;
+
+        // Changing bar width
+        targetBarWidth = round(map(this.value, new_min * 0.9, new_max * 1.1, 100, plotWidth - this.layout.pad));
+        // If the new bar width is greater than the current bar width
         if (targetBarWidth >= currentBarWidth) {
             widthChangeDirection = 1;
         }
+        // If the new bar width is less than the current bar width
         else {
             widthChangeDirection = -1;
         }
+
+        //Changing bar Y position
         targetY = y_positions[this.position] - barHeight / 2;
+        // If the new Y position is greater than the current Y position
         if (targetY >= currentY) {
             yChangeDirection = 1;
         }
+        // If the new Y position is less than the current Y position
         else {
             yChangeDirection = -1;
         }
     }
 
-    function get_bar_height(y_positions) {
+    // Generate the bar height based on the number of bars and the plot height
+    function get_bar_height() {
         var barHeight;
         var spacing = (y_positions[y_positions['last']] - y_positions[y_positions['first']]) / (y_positions['last'] - 1)
 
@@ -88,13 +118,5 @@ function Bar(_name, _value, _position, min, max, layout, y_positions) {
             barHeight = 25;
         }
         return barHeight;
-    }
-
-    // Checks if the mouse is hovering over the bar
-    // Input: x and y coordinates of the Bar object and the width and height of the bar
-    function checkMouseHover(x, y, width, height) {
-        if (mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height) {
-            return true;
-        }
     }
 }
--- visualizations/gdp_bar.js
+++ visualizations/gdp_bar.js
@@ -5,28 +5,20 @@ function GDP() {
 
     this.loaded = false;
 
-    // Public attributes
+    var self = this;
+
+    // Array of bar objects
     this.countriesBarArr = [];
+    // Object for accessing bar data based on ranking (e.g. 1: [country, gdp], 2: [country, gdp], etc.])
     this.barData;
     this.first_position = 1;
     this.last_position;
+    this.currentYear;
 
-    // Private attributes
-    var years = [];
-    var currentYear;
+    // Data structure for accessing Y positions based on ranking using key-value pairs (e.g. 1: 100, 2: 200, etc.)
     var ranking_y_positions;
-    var minGDP;
-    var maxGDP;
 
-    this.preload = function() {
-        var self = this;
-        this.data = loadTable(
-            'data/gdp/gdp_per_capita.csv', 'csv', 'header',
-            function(table) {
-                self.loaded = true;
-            });
-    }
-
+    // Chart layout intialization
     var marginSize = 35;
     this.layout = {
         marginSize: marginSize,
@@ -49,55 +41,53 @@ function GDP() {
 
     };
 
-    this.setup = function() {
-        console.log("World GDP setup initiated");
-
-        // Create year array
-        for (var i = 2; i < this.data.getColumnCount() - 1; i++) {
-            var y = Number(this.data.columns[i]);
-            years.push(y);
-        }
-
-        // Create ui HTML div
-        this.ui = createDiv().id('ui');
-        this.ui.parent('app');
-
-        var title = createP().parent('ui').html("<b>Controls</b>");
-
-        // Year slider
-        var yearDiv = createDiv().parent('ui').html("Select Year:");
-        let firstYear = years[0];
-        let lastYear = years[years.length - 1];
-        this.yearSlider = createSlider(firstYear, lastYear, firstYear, 1).parent(yearDiv);
-        currentYear = this.yearSlider.value();
-
-        // Number of bars selector
-
-        var numBarsDiv = createDiv().parent('ui').html("Select Number of Countries:");
-        this.numBarsSelector = createSelect().parent(numBarsDiv);
-        for(let i = 1; i<= 25; i++) {
-            this.numBarsSelector.option(i);
-        }
-        this.numBarsSelector.selected(10);
-        this.numBarsSelector.changed(this.change_num_bars.bind(this));
-
-        // Bar chart parameters (Showing top X number of countries countries, adjust accordingly);
-        this.last_position = this.numBarsSelector.value();
-        ranking_y_positions = get_ranking_y_pos(this.first_position, this.last_position, this.layout);
-
-        this.data_year_query(currentYear, this.first_position, this.last_position);
-    };
+    this.preload = function() {
+        var self = this;
+        this.data = loadTable(
+            'data/gdp/gdp_per_capita.csv', 'csv', 'header',
+            function(table) {
+                self.loaded = true;
+            });
+    }
     
     this.destroy = function() {
         console.log("in destroy");
         this.ui.remove();
     };
 
-    this.change_num_bars= function() {
+    this.setup = function() {
+        console.log("World GDP setup initiated");
+
+        // Create ui HTML div
+        this.ui = createDiv().id('ui');
+        this.ui.parent('app');
+
+        // Create title 
+        var title = createP().parent('ui').html("<b>Controls</b>");
+
+        // Year slider
+        let firstYear = this.data.columns[2];
+        let lastYear = this.data.columns[this.data.getColumnCount() -2];
+        var yearDiv = createDiv().parent('ui').html("Select Year: " + firstYear + " - " + lastYear);
+        this.yearSlider = createSlider(firstYear, lastYear, firstYear, 1).parent(yearDiv);
+        this.currentYear = this.yearSlider.value();
+
+        // Number of bars selector
+        var numBarsDiv = createDiv().parent('ui').html("Select Number of Countries:");
+        this.numBarsSelector = createSelect().parent(numBarsDiv);
+        for(let i = 1; i<= 25; i++) {
+            this.numBarsSelector.option(i);
+        }
+        this.numBarsSelector.selected(15);
+        this.numBarsSelector.changed(change_num_bars);
+
+        // Bar chart parameters (Showing top X number of countries countries, adjust accordingly);
         this.last_position = this.numBarsSelector.value();
         ranking_y_positions = get_ranking_y_pos(this.first_position, this.last_position, this.layout);
-        this.data_year_query(currentYear, this.first_position, this.last_position);
-    }
+
+        // Initial data query
+        this.data_year_query(this.currentYear, this.first_position, this.last_position);
+    };
 
     this.draw = function() {
         if (!this.loaded) {
@@ -106,23 +96,27 @@ function GDP() {
         }
     
         // Update year if there is a change
-        if (currentYear != this.yearSlider.value()) {
+        if (this.currentYear != this.yearSlider.value()) {
             this.data_year_query(this.yearSlider.value(), this.first_position, this.last_position);
-            currentYear = this.yearSlider.value();
+            this.currentYear = this.yearSlider.value();
         }
 
         // Bar chart elements
-        drawTitle(this.last_position, currentYear, this.layout);
+        drawTitle(this.last_position, this.currentYear, this.layout);
         drawAxis(this.layout);
         drawAxisLabels("GDP per Capita", "Ranking", this.layout);
         drawYAxisTickLables(ranking_y_positions, this.layout);
 
+        // Update and draw bars
         for (var i = 0; i < this.countriesBarArr.length; i++) {
             this.countriesBarArr[i].update();
             this.countriesBarArr[i].draw();
         }
     };
 
+    ///////////////////////////////////////
+    // Chart components helper functions //
+    ///////////////////////////////////////
     function drawTitle(numOfCountries, year, layout) {
         fill(0);
         noStroke();
@@ -135,10 +129,15 @@ function GDP() {
     };
 
     function drawYAxisTickLables(ranking_positions, layout) {
-        for (let i = ranking_positions['first']; i <= ranking_positions['last']; i++) {
+        for (let i = self.first_position; i <= self.last_position; i++) {
             text(i, layout.leftMargin - 20, ranking_positions[i]);
         }
     }
+
+
+    /////////////////////////////
+    // Other Bar chart methods //
+    /////////////////////////////
 
     // Maps the Y coordinates of the ranking lables
     // Input: First positon, Last Position, chart layout
@@ -153,8 +152,8 @@ function GDP() {
         return ranking_positions;
     }
 
-    // Gets the top 10 countries with the highest gdp for a given year
-    // Updates the top10Data and countriesBarArr
+    // Gets the data of the countries from the first to last positons based on the year
+    // Updates the barData and countriesBarArr attributes
     this.data_year_query = function(year, first_position, last_position) {
         var countries = this.data.getColumn('Country Name');
         var gdp = this.data.getColumn(year.toString());
@@ -163,10 +162,10 @@ function GDP() {
         var pairs = countries.map((name, index) => [name, gdp[index]]);
         // Sort the pairs by gdp (second element in each pair) in decending order
         pairs.sort((a, b) => b[1] - a[1]);
-
         // Get the data of the countries from the first to last positons
         var selectedPairs = pairs.slice(first_position - 1, last_position);
 
+        // Create an object with the data of the countries from the first to last positons
         this.barData = {};
         var counter = 0;
         for(let i = first_position; i <= last_position; i++) {
@@ -174,10 +173,10 @@ function GDP() {
             counter++;
         }
 
-        minGDP = this.barData[last_position][1];
-        maxGDP = this.barData[first_position][1];
+        var minGDP = this.barData[last_position][1];
+        var maxGDP = this.barData[first_position][1];
 
-        // Create a bar object for each of the top 10 countries and push it to an array
+        // Create a bar object for each of the top X countries and push it to an array if there is no pre-existing array
         // Or if number of bars has changed
         if (this.countriesBarArr.length == 0 || this.countriesBarArr.length != last_position) {
             var barArr = [];
@@ -187,6 +186,7 @@ function GDP() {
             }
             this.countriesBarArr = barArr;
         }
+        // Update existing bar objects and create new ones if there are new countries
         else {
             // newBarArray is the array for the new countriesBarArr
             var newBarArray = [];
@@ -200,6 +200,7 @@ function GDP() {
                         break;
                     }
                 }
+                // No pre-existing bar object found for the country
                 if (!found) {
                     let bar = new Bar(this.barData[i][0], this.barData[i][1], i, minGDP, maxGDP, this.layout, ranking_y_positions);
                     newBarArray.push(bar);
@@ -207,6 +208,13 @@ function GDP() {
             }
             this.countriesBarArr = newBarArray;
         }
+    }
+
+    // Change the number of bars displayed
+    function change_num_bars() {
+        self.last_position = self.numBarsSelector.value();
+        ranking_y_positions = get_ranking_y_pos(self.first_position, self.last_position, self.layout);
+        self.data_year_query(self.currentYear, self.first_position, self.last_position);
     }
 }
 
